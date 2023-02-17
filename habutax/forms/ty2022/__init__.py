6d132db305c8903e2830029adf57ad81453c1a13
Aaron Lindsay <aaron@aclindsay.com>
2023-02-16 22:05:32 -0500

    Initialize tax year 2022 by importing 2021 forms
    Do *not* yet enable them to be used by the top-level scripts since they
    are incorrect - this is intended to be a stepping stone to make it
    easier to work on the 2022 forms one-by-one.

---
+++ habutax/forms/ty2022/__init__.py
@@ -0,0 +1,51 @@
+from ..ty2021.f1040 import Form1040
+from ..ty2021.f1040_qualdiv_capgain_tax_wkst import Form1040QualDivCapGainTaxWkst
+from ..ty2021.f1040_recovery_rebate_credit_wkst import Form1040RecoveryRebateCreditWkst
+from ..ty2021.f1040_s2_need6251 import Form1040S2Need6251
+from ..ty2021.f1040_s1 import Form1040S1
+from ..ty2021.f1040_s3 import Form1040S3
+from ..ty2021.f1040_sa import Form1040SA
+from ..ty2021.f1040_sb import Form1040SB
+from ..ty2021.f1040_s8812 import Form1040S8812
+from ..ty2021.f1098 import Form1098
+from ..ty2021.f1099_div import Form1099DIV
+from ..ty2021.f1099_g import Form1099G
+from ..ty2021.f1099_int import Form1099INT
+from ..ty2021.f1099_r import Form1099R
+from ..ty2021.f8606 import Form8606
+from ..ty2021.f8959 import Form8959
+from ..ty2021.f8995 import Form8995
+from ..ty2021.fw_2 import FormW2
+from ..ty2021.f8889 import Form8889
+from ..ty2021.fnc_d_400 import FormNCD400
+from ..ty2021.fnc_d_400_child_deduction_wkst import FormNCD400ChildDeductionWkst
+from ..ty2021.fnc_d_400_consumer_use_tax_wkst import FormNCD400ConsumerUseTaxWkst
+from ..ty2021.fnc_d_400_ss import FormNCD400SS
+from ..ty2021.fnc_d_400_sa import FormNCD400SA
+
+available_forms = [
+	Form1040,
+	Form1040QualDivCapGainTaxWkst,
+	Form1040RecoveryRebateCreditWkst,
+	Form1040S1,
+	Form1040S2Need6251,
+	Form1040S3,
+	Form1040SA,
+	Form1040SB,
+	Form1040S8812,
+	Form1098,
+	Form1099DIV,
+	Form1099G,
+	Form1099INT,
+	Form1099R,
+	Form8606,
+	Form8959,
+	Form8995,
+	FormW2,
+	Form8889,
+	FormNCD400,
+	FormNCD400ChildDeductionWkst,
+	FormNCD400ConsumerUseTaxWkst,
+	FormNCD400SS,
+	FormNCD400SA,
+]
---
+++ tests/ty2022/test_f1040.py
@@ -0,0 +1,27 @@
+import unittest
+
+from ..test_form import FormTestCase
+
+import habutax.forms.ty2022 as ty2022
+
+class Form1040SimpleTestCase(FormTestCase):
+    def setUp(self):
+        self.fixture_setup(ty2022.available_forms, input_fixture='tests/ty2022/fixtures/f1040_one_w2.habutax')
+
+    def test_simple_1040(self):
+        solution = self.assertSolve(['1040'])
+
+        self.assertIn('1040', solution)
+        self.assertIn('w-2:0', solution)
+
+        self.assertDollarsEqual(solution, '1040.1', 100000)
+        self.assertDollarsEqual(solution, '1040.12c', 12550 + 300)
+        self.assertDollarsEqual(solution, '1040.15', 100000 - (12550 + 300))
+        self.assertDollarsEqual(solution, '1040.16', 14943)
+        self.assertDollarsEqual(solution, '1040.25a', 16551.9)
+        self.assertDollarsEqual(solution, '1040.25d', 16551.9)
+        self.assertDollarsEqual(solution, '1040.33', 16551.9)
+        self.assertDollarsEqual(solution, '1040.34', 1608.9)
+        self.assertDollarsEqual(solution, '1040.35a', 1608.9)
+        self.assertDollarsEqual(solution, '1040.37', 0)
+        self.assertDollarsEqual(solution, '1040.38', 0)
