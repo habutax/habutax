a6fbb14de417cb521963e649b9d2dec79e94bddf
Aaron Lindsay <aaron@aclindsay.com>
2023-03-06 09:16:39 -0500

    TY2022: Adjust simple 1040 test case for tax year

--- tests/ty2022/test_f1040.py
+++ tests/ty2022/test_f1040.py
@@ -14,14 +14,16 @@ class Form1040SimpleTestCase(FormTestCase):
         self.assertIn('1040', solution)
         self.assertIn('w-2:0', solution)
 
-        self.assertDollarsEqual(solution, '1040.1', 100000)
-        self.assertDollarsEqual(solution, '1040.12c', 12550 + 300)
-        self.assertDollarsEqual(solution, '1040.15', 100000 - (12550 + 300))
-        self.assertDollarsEqual(solution, '1040.16', 14943)
-        self.assertDollarsEqual(solution, '1040.25a', 16551.9)
-        self.assertDollarsEqual(solution, '1040.25d', 16551.9)
-        self.assertDollarsEqual(solution, '1040.33', 16551.9)
-        self.assertDollarsEqual(solution, '1040.34', 1608.9)
-        self.assertDollarsEqual(solution, '1040.35a', 1608.9)
-        self.assertDollarsEqual(solution, '1040.37', 0)
-        self.assertDollarsEqual(solution, '1040.38', 0)
+        self.assertDollarsEqual(solution, '1040.1a', 100000) # W-2 income
+        self.assertDollarsEqual(solution, '1040.11', 100000) # AGI
+        self.assertDollarsEqual(solution, '1040.12', 12950)  # standard deduction
+        self.assertDollarsEqual(solution, '1040.15', 100000 - 12950) # taxable income
+        self.assertDollarsEqual(solution, '1040.16', 14774) # tax
+        self.assertDollarsEqual(solution, '1040.24', 14774) # total tax
+        self.assertDollarsEqual(solution, '1040.25a', 16551.90) # W-2 withholding
+        self.assertDollarsEqual(solution, '1040.25d', 16551.90) # total withholding
+        self.assertDollarsEqual(solution, '1040.33', 16551.90) # total payments
+        self.assertDollarsEqual(solution, '1040.34', 1777.90) # overpayment
+        self.assertDollarsEqual(solution, '1040.35a', 1777.90) # refund
+        self.assertDollarsEqual(solution, '1040.37', 0) # amount you owe
+        self.assertDollarsEqual(solution, '1040.38', 0) # tax penalty
