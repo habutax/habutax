5cfc21da2030d653144d1ecbd2ca7049305cbb06
Aaron Lindsay <aaron@aclindsay.com>
2023-03-06 09:18:29 -0500

    TY2022: Begin using most 2022 forms

--- habutax/forms/ty2022/__init__.py
+++ habutax/forms/ty2022/__init__.py
@@ -1,32 +1,30 @@
-from ..ty2021.f1040 import Form1040
+from .f1040 import Form1040
 from .f1040_qualdiv_capgain_tax_wkst import Form1040QualDivCapGainTaxWkst
-from ..ty2021.f1040_recovery_rebate_credit_wkst import Form1040RecoveryRebateCreditWkst
-from ..ty2021.f1040_s2_need6251 import Form1040S2Need6251
-from ..ty2021.f1040_s1 import Form1040S1
-from ..ty2021.f1040_s3 import Form1040S3
-from ..ty2021.f1040_sa import Form1040SA
-from ..ty2021.f1040_sb import Form1040SB
-from ..ty2021.f1040_s8812 import Form1040S8812
+from .f1040_s2_need6251 import Form1040S2Need6251
+from .f1040_s1 import Form1040S1
+from .f1040_s3 import Form1040S3
+from .f1040_sa import Form1040SA
+from .f1040_sb import Form1040SB
+from .f1040_s8812 import Form1040S8812
 from ..ty2021.f1098 import Form1098
 from ..ty2021.f1099_div import Form1099DIV
 from ..ty2021.f1099_g import Form1099G
 from ..ty2021.f1099_int import Form1099INT
 from ..ty2021.f1099_r import Form1099R
-from ..ty2021.f8606 import Form8606
-from ..ty2021.f8959 import Form8959
-from ..ty2021.f8995 import Form8995
+from .f8606 import Form8606
+from .f8959 import Form8959
+from .f8995 import Form8995
 from ..ty2021.fw_2 import FormW2
-from ..ty2021.f8889 import Form8889
-from ..ty2021.fnc_d_400 import FormNCD400
-from ..ty2021.fnc_d_400_child_deduction_wkst import FormNCD400ChildDeductionWkst
-from ..ty2021.fnc_d_400_consumer_use_tax_wkst import FormNCD400ConsumerUseTaxWkst
-from ..ty2021.fnc_d_400_ss import FormNCD400SS
-from ..ty2021.fnc_d_400_sa import FormNCD400SA
+from .f8889 import Form8889
+from .fnc_d_400 import FormNCD400
+from .fnc_d_400_child_deduction_wkst import FormNCD400ChildDeductionWkst
+from .fnc_d_400_consumer_use_tax_wkst import FormNCD400ConsumerUseTaxWkst
+from .fnc_d_400_ss import FormNCD400SS
+from .fnc_d_400_sa import FormNCD400SA
 
 available_forms = [
 	Form1040,
 	Form1040QualDivCapGainTaxWkst,
-	Form1040RecoveryRebateCreditWkst,
 	Form1040S1,
 	Form1040S2Need6251,
 	Form1040S3,
