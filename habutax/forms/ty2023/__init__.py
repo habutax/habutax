85080e998747ec2efaed8bac5bf02b624140401b
Aaron Lindsay <aaron@aclindsay.com>
2024-02-03 22:45:11 -0500

    TY2023: Add initial __init__.py

---
+++ habutax/forms/ty2023/__init__.py
@@ -0,0 +1,27 @@
+from .f1040 import Form1040
+from .f1040_qualdiv_capgain_tax_wkst import Form1040QualDivCapGainTaxWkst
+from .f1040_s1 import Form1040S1
+from .f1040_s2_need6251 import Form1040S2Need6251
+from .f1040_s3 import Form1040S3
+from .f1040_sa import Form1040SA
+from .f1040_sb import Form1040SB
+from .f1040_s8812 import Form1040S8812
+from .f8606 import Form8606
+from .f8959 import Form8959
+from .f8995 import Form8995
+from .f8889 import Form8889
+
+available_forms = [
+    Form1040,
+    Form1040QualDivCapGainTaxWkst,
+    Form1040S1,
+    Form1040S2Need6251,
+    Form1040S3,
+    Form1040SA,
+    Form1040SB,
+    Form1040S8812,
+    Form8606,
+    Form8959,
+    Form8995,
+    Form8889,
+]
