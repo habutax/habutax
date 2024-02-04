189cd63b0ed3b27455c1cd4d70211013c765bccc
Aaron Lindsay <aaron@aclindsay.com>
2024-02-03 21:31:02 -0500

    TY2023: Separate out thresholds for Form 1040, Qualified Dividends and Capital Gain Tax Worksheet

--- habutax/forms/ty2023/f1040_qualdiv_capgain_tax_wkst.py
+++ habutax/forms/ty2023/f1040_qualdiv_capgain_tax_wkst.py
@@ -1,4 +1,4 @@
-from habutax.enum import filing_status
+from habutax.enum import filing_status as status
 from habutax.form import Form, Jurisdiction
 from habutax.inputs import *
 from habutax.fields import *
@@ -13,33 +13,23 @@ class Form1040QualDivCapGainTaxWkst(Form):
     jurisdiction = Jurisdiction.US
 
     def __init__(self, **kwargs):
+        thresholds = {
+            'line_6': {
+                (status.Single, status.MarriedFilingSeparately):                 44625.0,
+                (status.MarriedFilingJointly, status.QualifyingSurvivingSpouse): 89250.0,
+                status.HeadOfHousehold:                                          59750.0,
+            },
+            'line_13': {
+                status.Single:                                                   492300.0,
+                status.MarriedFilingSeparately:                                  276900.0,
+                (status.MarriedFilingJointly, status.QualifyingSurvivingSpouse): 553850.0,
+                status.HeadOfHousehold:                                          523050.0
+            },
+        }
+
         inputs = [
             BooleanInput('form_2555', description="Do you need to fill out Form 2555 (relating to foreign earned income)?"),
         ]
-
-        def line_6(self, i, v):
-            # threshold
-            if i['1040.filing_status'] in [filing_status.Single, filing_status.MarriedFilingSeparately]:
-                return 44625.0
-            elif i['1040.filing_status'] in [filing_status.MarriedFilingJointly, filing_status.QualifyingSurvivingSpouse]:
-                return 89250.0
-            elif i['1040.filing_status'] == filing_status.HeadOfHousehold:
-                return 59750.0
-            else:
-                self.not_implemented()
-
-        def line_13(self, i, v):
-            # threshold
-            if i['1040.filing_status'] is filing_status.Single:
-                return 492300.0
-            if i['1040.filing_status'] is filing_status.MarriedFilingSeparately:
-                return 276900.0
-            elif i['1040.filing_status'] in [filing_status.MarriedFilingJointly, filing_status.QualifyingSurvivingSpouse]:
-                return 553850.0
-            elif i['1040.filing_status'] == filing_status.HeadOfHousehold:
-                return 523050.0
-            else:
-                self.not_implemented()
 
         optional_fields = [
             FloatField('1', lambda s, i, v: s.not_implemented() if i['form_2555'] else v['1040.15']),
@@ -47,14 +37,14 @@ class Form1040QualDivCapGainTaxWkst(Form):
             FloatField('3', lambda s, i, v: s.not_implemented() if i['1040.schedule_d_required'] else v['1040.7']),
             FloatField('4', lambda s, i, v: v['2'] + v['3']),
             FloatField('5', lambda s, i, v: max(0.0, v['1'] - v['4'])),
-            FloatField('6', line_6),
+            FloatField('6', lambda s, i, v: s.threshold('line_6')),
             FloatField('7', lambda s, i, v: min(v['1'], v['6'])),
             FloatField('8', lambda s, i, v: min(v['5'], v['7'])),
             FloatField('9', lambda s, i, v: v['7'] - v['8']),
             FloatField('10', lambda s, i, v: min(v['1'], v['4'])),
             FloatField('11', lambda s, i, v: v['9']),
             FloatField('12', lambda s, i, v: v['10'] - v['11']),
-            FloatField('13', line_13),
+            FloatField('13', lambda s, i, v: s.threshold('line_13')),
             FloatField('14', lambda s, i, v: min(v['1'], v['13'])),
             FloatField('15', lambda s, i, v: v['5'] + v['9']),
             FloatField('16', lambda s, i, v: max(0.0, v['14'] - v['15'])),
@@ -69,7 +59,8 @@ class Form1040QualDivCapGainTaxWkst(Form):
             FloatField('25', lambda s, i, v: min(v['23'], v['24'])),
         ]
 
-        super().__init__(__class__, inputs, [], optional_fields, **kwargs)
+        super().__init__(__class__, inputs, [], optional_fields,
+                         thresholds=thresholds, **kwargs)
 
     def needs_filing(self, values):
         return False
