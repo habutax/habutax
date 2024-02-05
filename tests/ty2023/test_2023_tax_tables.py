556ef0c6c8bb5b8329b793f496677a3ff5a7c133
Aaron Lindsay <aaron@aclindsay.com>
2024-02-04 22:04:49 -0500

    TY2023: Update remaining 2022 tests for 2023

--- tests/ty2023/test_2023_tax_tables.py
+++ tests/ty2023/test_2023_tax_tables.py
@@ -1,7 +1,5 @@
 import unittest
 
-from .. import context
-
 from habutax.forms.ty2023.f1040_figure_tax import figure_tax
 from habutax import enum
 
@@ -9,6 +7,7 @@ class FigureTaxTestCase(unittest.TestCase):
     def test_figure_2023_1040_tax(self):
         scenarios = (
             (75047.08, enum.filing_status.Single, 11813),
+            (100000.00, enum.filing_status.Single, 17400),
             (98325.84, enum.filing_status.MarriedFilingJointly, 12247),
             (99999.99, enum.filing_status.HeadOfHousehold, 15788),
             (100000.00, enum.filing_status.HeadOfHousehold, 15794),
