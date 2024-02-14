586428a907039a890dd3be88cedc23262b741792
Aaron Lindsay <aaron@aclindsay.com>
2024-02-13 22:12:38 -0500

    Enable 2023 tax year

--- habutax/forms/__init__.py
+++ habutax/forms/__init__.py
@@ -1,7 +1,9 @@
 from habutax.forms import ty2021
 from habutax.forms import ty2022
+from habutax.forms import ty2023
 
 available_forms = {
     2021: ty2021.available_forms,
     2022: ty2022.available_forms,
+    2023: ty2023.available_forms,
 }
